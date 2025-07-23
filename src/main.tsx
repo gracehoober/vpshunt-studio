c15676a5f0b151dae308612d51decb088b7a51f9
Grace Bethany Hoober <grace.hoober66@gmail.com>
2025-07-23 16:52:06 -0700

    feat: :sparkles: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router";
 import theme from "./themes/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>,
 );
