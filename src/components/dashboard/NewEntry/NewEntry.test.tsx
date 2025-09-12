0b0f1d32501ff219aa6c3794ad70589c4d4afea0
Grace Bethany Hoober <grace.hoober66@gmail.com>
2025-09-12 14:57:57 -0700

    test: :construction: NewEntry testing in progress

--- src/components/dashboard/NewEntry/NewEntry.test.tsx
+++ src/components/dashboard/NewEntry/NewEntry.test.tsx
@@ -1,10 +1,41 @@
-import { render } from "@testing-library/react";
+import { getByRole, render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 import { NewEntry } from "./NewEntry";
 
 describe("<App>", () => {
   it("renders successfully", () => {
-    render(<NewEntry onClose={() => false} open={false} />);
+    render(<NewEntry onClose={() => false} open={true} />);
   });
+
+  it("matches snapshot", () => {
+    const { container } = render(
+      <NewEntry onClose={() => false} open={false} />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it("contains the text 'Add New Entry' button", () => {
+    const {} = render(<NewEntry onClose={() => false} open={false} />);
+    expect(getByText("addEntry.button")).toBeInTheDocument();
+  });
+
+  // it("contains a form element", () => {
+  //   const { container } = render(
+  //     <NewEntry onClose={() => false} open={false} />,
+  //   );
+  //   const form = container.querySelector("form");
+  //   expect(form).toBeInTheDocument();
+  // });
+
+  // it("contains input fields", () => {
+  //   const { getByLabelText } = render(
+  //     <NewEntry onClose={() => false} open={false} />,
+  //   );
+  //   expect(getByLabelText("Patient ID")).toBeInTheDocument();
+  //   expect(getByLabelText("addEntry.fields.ipAddress")).toBeInTheDocument();
+  //   expect(getByLabelText("addEntry.fields.username")).toBeInTheDocument();
+  //   expect(getByLabelText("addEntry.fields.password")).toBeInTheDocument();
+  //   expect(getByLabelText("addEntry.fields.sshKey")).toBeInTheDocument();
+  // });
 });
